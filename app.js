9a724e00a6608a62dd9396e52ebda0ea54954368
Michał Knieć <playforgame97@gmail.com>
2020-10-28 12:33:21 +0100

    open beta

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let birdBottom = 200
     let gravity = 2
     let isGameOver = false
+    let gap = 440
 
     function startGame() {
 
@@ -27,26 +28,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function generateObstacle() {
         let obstacleLeft = 500
-        let randomHeight = Math.random() * 200
+        let randomHeight = Math.random() * 150
+        console.log(randomHeight)
         let obstacleBottom = randomHeight
         const obstacle = document.createElement('div')
-        obstacle.classList.add('obstacle')
+        const topObstacle = document.createElement('div')
+        if (!isGameOver) {
+            topObstacle.classList.add('topObstacle')
+            obstacle.classList.add('obstacle')
+        }
         gameDisplay.appendChild(obstacle)
+        gameDisplay.appendChild(topObstacle)
+        topObstacle.style.left = obstacleLeft + 'px'
         obstacle.style.left = obstacleLeft + 'px'
         obstacle.style.bottom = obstacleBottom + 'px'
+        topObstacle.style.bottom = obstacleBottom + gap + 'px'
 
         function moveObstacle() {
             let speed = 2
             obstacleLeft -= speed
-            obstacle.style.left = obstacleLeft + 'px';
+            obstacle.style.left = obstacleLeft + 'px'
+            topObstacle.style.left = obstacleLeft + 'px'
 
             if (obstacleLeft === -60) {
                 clearInterval(speedId)
                 gameDisplay.removeChild(obstacle)
+                gameDisplay.removeChild(topObstacle)
             }
-            if (birdBottom === 0) {
+            if (
+                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 && (birdBottom < obstacleBottom + 151 || birdBottom > obstacleBottom + gap -220) ||
+                birdBottom === 0
+            ) {
                 gameOver()
-                
+                clearInterval(speedId)
             }
         }
         let speedId = setInterval(moveObstacle, 20)
@@ -55,6 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
     generateObstacle()
 
     function gameOver() {
+        console.log('gameover')
         clearInterval(timerId)
         isGameOver = true
         document.removeEventListener('click', jump)
